1e5de5c65b8ac7d3bf036314d97760495e76e0e3
Witt Allen <wittionary@users.noreply.github.com>
2024-03-20 22:56:03 -0500

    Added pretty print for json; starting sqlite3 code

--- add.py
+++ add.py
@@ -1,6 +1,8 @@
 # CLI tool that lets you quickly add items to storage
 
 import sys, json, random
+import sqlite3 
+from sqlite3 import Error
 
 OBJECTS_FILE = "objects.json"
 
@@ -24,7 +26,7 @@ def save_object():
     try: # read from existing file
         with open(OBJECTS_FILE, 'r') as file:
             json_data = json.load(file)
-            print(f"File loaded")
+            # print(f"File loaded")
     except:
         print(f"File `{OBJECTS_FILE}` not loaded into memory")
 
@@ -34,19 +36,27 @@ def save_object():
     #print(f"JSON + OBJECT: {object}")
 
     with open(OBJECTS_FILE, 'w') as file:
-        json.dump(object, file)
-        print(f"File saved! \n\t {len(object)} entries present")
+        json.dump(object, file, indent=4)
+        print(f"Item added\n\t {len(object)} entries present")
 
 def list_objects():
     """Output what's saved to disk"""
+    try: # read from existing file
+        with open(OBJECTS_FILE, 'r') as file:
+            json_data = json.load(file)
+            print(f"File loaded")
+    except:
+        print(f"File `{OBJECTS_FILE}` not loaded into memory")
+
+    print(json.dumps(json_data, indent=4, sort_keys=True))
 
 def attach_entry_id(object):
-    # random 6 character numeric id
+    """Adds a random 6-character numeric id to the entry"""
     number = random.randrange(999999)
     entry_id = str(number).zfill(6)
-    #print(f"ENTRY ID: {entry_id}")
+    
     entry = {"id": entry_id, "entry": object}
-    print(f"ENTRY OBJECT: {entry}")
+    # print(f"ENTRY OBJECT: {entry}")
 
     return entry
 
